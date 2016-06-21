f08eac03246e0851197bca4035880fd458097431
Steve Kiesling <steve.kiesling@gmail.com>
2016-06-21 07:30:28 -0700

    layout, bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'index#welcome'
+  #get 'pages/home' => 'high_voltage/pages#show', id: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
